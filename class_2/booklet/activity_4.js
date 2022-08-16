d5bb0a3c143c4dca96102d4123dfa83dc6e98362
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-08-16 17:42:40 -0300

    :construction: Add class 2 - activity 4

---
+++ class_2/booklet/activity_4.js
@@ -0,0 +1,20 @@
+/*
+ACTIVIDAD 4
+PRESUPUESTO
+
+Solicitar al usuario un (1) número.
+Si el valor está entre dos números, efectuar una de las siguientes salidas, según corresponda:
+- “Presupuesto bajo” si está entre 0 y 1000.
+- “Presupuesto medio” si está entre 1001 y 3000.
+- “Presupuesto alto” si es  mayor que 3000.
+
+*/
+
+let budget = parseFloat(prompt("Enter your budget"));
+if (0 <= budget <= 1000) {
+  alert("Low budget");
+} else if (1001 <= budget <= 3000) {
+  alert("Medium budget");
+} else if (budget > 3000) {
+  alert("High budget");
+}
