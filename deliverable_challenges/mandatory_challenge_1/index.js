76524813da636789c727efcb12aa9ae5d41972dc
ezeBalsamo <alexander.ezequiel.balsamo@gmail.com>
2022-08-30 20:55:25 -0300

    :bulb: Delete simulator comment

--- deliverable_challenges/mandatory_challenge_1/index.js
+++ deliverable_challenges/mandatory_challenge_1/index.js
@@ -1,14 +1,3 @@
-/*
-This script simulates the completion of a sticker album.
-In order to do that, the player has to enter the money that he/she is willing to spend.
-Then, he/she will have to decide how many packs wants to buy, until:
-A) The album is completed
-B) He/she run out of money
-C) The remaining money is not enough to complete the album
-
-Of course, this simulation considers that the probability of obtaining new cards decreases as the album is completed.
-*/
-
 const TOTAL_NUMBER_OF_STICKERS_IN_ALBUM = 638;
 const NUMBER_OF_STICKERS_PER_PACK = 5;
 const PACK_PRICE = 150;
